e9b0817948c78d298fe5312b6eedb2b93b68ead4
Antonio Monteiro <antoniomonteiro@ioasys.com.br>
2019-11-11 14:36:26 -0300

    Modificações simples

--- app/src/main/java/br/com/empresas_tests/SplashActivity.kt
+++ app/src/main/java/br/com/empresas_tests/SplashActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.app.ComponentActivity
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.navigation.findNavController
 
 class SplashActivity : AppCompatActivity() {
 
